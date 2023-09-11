9a464c1aadf135d24e2126c6edab2c9c12c41cbe
xinxin hu <cynthiahu3727@gmail.com>
2023-09-11 09:28:06 -0400

    add commit c2 to rebase branch

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,6 @@
 # this program prints Hello, world!
 print('Hello, world! Cynthia has studied in UofT for three years')
+print('add commit c1 to rebase branch')
+print('add commit c2 to rebase branch')
+
 

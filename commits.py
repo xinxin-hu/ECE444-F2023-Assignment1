75dcf1aada54a45091bd95a39a6b0918dfdbefaa
xinxin hu <cynthiahu3727@gmail.com>
2023-09-11 10:00:33 -0400

    add commit c3 to develop branch

---
+++ commits.py
@@ -0,0 +1,10 @@
+def process_word(word):
+    return word == "please"
+
+if __name__ == '__main__':
+    print("What is the magic word?")
+    magic_word = "please"
+    if process_word(magic_word):
+        print("yes ",magic_word, " is the magic word")
+    else:
+        print("no that is not the magic word")
